6aa5745104b4512dfcb7ec21ef60175e76e72561
Sebahattin ÖZDEMİR <sebahattinozdmr@gmail.com>
2022-04-08 09:36:49 +0300

    react-query-example

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,3 +1,52 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
-}
+  // used when open a modal or navigating the other page depends on the data status data is injected automotically when onSuccess
+  const onSuccess = (data) => {
+    console.log("Succesfuly gets the data", data);
+  };
+
+  const onError = (error) => {
+    console.log("Error while getting data", error);
+  };
+
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
+    "super-heroes",
+    fetchSuperHeroes,
+    {
+      cacheTime: 5000, // cacheTime is defaul 5 muntes
+      staleTime: 5000, // use it when data does not change often it descreases the network request default 0
+      refetchOnMount: true, // default true whenever component changes network request happen
+      refetchOnWindowFocus: true, // async with real time data, default true whenever db changes ne request it is usded for realtime data
+      refetchInterval: 2000, // get data every two seconds
+      refetchIntervalInBackground: true, // to use it with refetchInterval Together
+      enabled: false, // prevents automatically to fetching data by default true, if it is false data will not come,
+      onSuccess: onSuccess,
+      onError: onError,
+      //data transformation,
+    }
+  );
+
+  if (isLoading || isFetching) {
+    return <h1>LOADING...</h1>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
+
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      <button onClick={refetch}>Fetch Data</button>
+      {data?.data.map((hero) => (
+        <p key={hero.name}>{hero.name}</p>
+      ))}
+    </>
+  );
+};
--- react-query-starter/src/components/SuperHeroes.page.js
+++ react-query-starter/src/components/SuperHeroes.page.js
@@ -1,27 +1,38 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export const SuperHeroesPage = () => {
-  const [isLoading, setIsLoading] = useState(true)
-  const [data, setData] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    axios.get('http://localhost:4000/superheroes').then(res => {
-      setData(res.data)
-      setIsLoading(false)
-    })
-  }, [])
+    axios
+      .get("http://localhost:4000/superheroes")
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setIsError(true);
+      });
+  }, []);
 
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
   if (isLoading) {
-    return <h2>Loading...</h2>
+    return <h2>Loading...</h2>;
   }
 
   return (
     <>
       <h2>Super Heroes Page</h2>
-      {data.map(hero => {
-        return <div>{hero.name}</div>
+      {data.map((hero) => {
+        return <div key={hero.id}>{hero.name}</div>;
       })}
     </>
-  )
-}
+  );
+};
